bc0bf5729b5e890e34a1324afce213af19739066
robin <robin@watcha.com>
2024-09-07 23:59:02 +0900

    Adapting and Extending

--- src/App.js
+++ src/App.js
@@ -4,24 +4,18 @@ const Father = styled.div`
     display: flex;
 `;
 const Box = styled.div`
+    background-color: ${(props) => props.bgColor};
     width: 100px;
     height: 100px;
 `;
-const BoxOne = styled(Box)`
-    background-color: teal;
-`;
-const BoxTwo = styled(Box)`
-    background-color: tomato;
-`;
-const Text = styled.span`
-    color: white;`;
+const Circle = styled(Box)`
+    border-radius: 50px;
+`
 
 function App() {
     return <Father>
-        <BoxOne>
-            <Text>Hello</Text>
-        </BoxOne>
-        <BoxTwo/>
+        <Box bgColor="teal"/>
+        <Circle bgColor="tomato"/>
     </Father>;
 }
 
