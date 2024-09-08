61d8eda313edbab1e35f9b40b319de9f8c605314
robin <robin@watcha.com>
2024-09-08 23:38:25 +0900

    Super Recap

--- src/App.js
+++ src/App.js
@@ -1,52 +1,31 @@
-import styled, {keyframes} from "styled-components";
+import styled from "styled-components";
 
-const Wraper = styled.div`
-    display: flex;
-`;
-const rotationAnimation = keyframes`
-    0% {
-        transform: rotate(0deg);
-        border-radius: 0px;
-    }
-    50% {
-        transform: rotate(360deg);
-        border-radius: 100px;
-    }
-    100% {
-        transform: rotate(0deg);
-        border-radius: 0px;
+const Title = styled.h1`
+    color: tomato;
+
+    &:hover {
+        color: teal;
     }
 `
-const Emoji = styled.p`
-    font-size: 36px;
-`
-const Box = styled.div`
-    height: 200px;
-    width: 200px;
-    background-color: tomato;
+const Wrapper = styled.div`
     display: flex;
+    height: 100vh;
+    width: 10vw;
     justify-content: center;
     align-items: center;
-    animation: ${rotationAnimation} 1s linear infinite;
 
-    ${Emoji} {
-        &:hover {
-            font-size: 98px;
-        }
-
-        &:active {
-            opacity: 0;
-        }
+    ${Title}:hover & {
+        font-size: 99px;
     }
-`
+`;
+
 
 function App() {
-    return <Wraper>
-        <Box>
-            <Emoji>ㅋ</Emoji>
-        </Box>
-            <Emoji>ㅎ</Emoji>
-    </Wraper>;
+    return <Wrapper>
+        <Title>
+            Hello
+        </Title>
+    </Wrapper>;
 }
 
 export default App;
