e0bec65d14eb3ebc75878d3d94940a5493b43270
robin <robin@watcha.com>
2024-11-25 00:30:53 +0900

    Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import React, {useState} from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    background-color: ${props => props.theme.bgColor};
+`;
+const Hi = styled.h1`
+    color: ${props => props.theme.textColor};
+`;
 
 function App() {
-    const [value, setValue] = useState("");
-    const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-        const {
-            currentTarget: {value},
-        } = event;
-        setValue(value);
-    };
-    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log("hello", value);
-    }
+
     return (
         <div>
-            <form onSubmit={onSubmit}>
-                <input value={value} onChange={onChange} type="text" placeholder="user_name"/>
-                <button>Log in</button>
-            </form>
+            <Container>
+                <Hi>protected</Hi>
+            </Container>
         </div>
     )
 }
